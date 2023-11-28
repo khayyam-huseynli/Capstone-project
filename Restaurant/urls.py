c0b131e635382d1e33c30911795b7e852075c5dd
khayyam-huseynli <khayyam.huseynli@gmail.com>
2023-11-28 14:03:59 +0400

    Securing the table booking API-isauthenticated permission

--- Restaurant/urls.py
+++ Restaurant/urls.py
@@ -1,8 +1,7 @@
-#define URL route for index() view
 from django.urls import path
 from . import views
 
 urlpatterns = [
     path('items/', views.MenuItemView.as_view()),
-    path('items/<int:pk>', views.SingleMenuItemView.as_view()),
+    path('items/<int:pk>', views.SingleMenuItemView.as_view()),    
 ]
--- Restaurant/views.py
+++ Restaurant/views.py
@@ -1,4 +1,5 @@
 from rest_framework import generics, viewsets
+from rest_framework.permissions import IsAuthenticated
 from .models import Menu, Booking
 from .serializers import MenuSerializer, BookingSerializer
 
@@ -13,8 +14,6 @@ class SingleMenuItemView(generics.RetrieveUpdateAPIView, generics.DestroyAPIView
 
 
 class BookingViewSet(viewsets.ModelViewSet):
+    permission_classes = [IsAuthenticated]
     queryset = Booking.objects.all()
     serializer_class = BookingSerializer
-
-
-
